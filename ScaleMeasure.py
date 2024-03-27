b5e6a5c53d6be4ff1901ac181bb306768aca4065
FungusDuder <160258885+CooperBoling@users.noreply.github.com>
2024-03-27 18:48:26 -0500

    Added 2 Methods + constructor
    Two methods: A "portionInfo" method which will take the weight and serving size of a nutrition value and convert it to the nutrition value in the portion. The second method is a __str__ skeleton. The constructor was filled out as well.

---
+++ ScaleMeasure.py
@@ -0,0 +1,25 @@
+class ScaleMeasure():
+    def __init__(self, weight: float, food: Food) -> None:
+        self.pWeight = weight
+        self.pSugars = self.portionInfo(food.sugars)
+        self.pCalories = self.portionInfo(food.calories)
+        self.pProtien = self.portionInfo(food.protien)
+        self.pCarbs = self.portionInfo(food.carbs)         #The "p" in front of the variable names
+        self.pFiber = self.portionInfo(food.fiber)          #stands for "portioned Sugars" etc.
+        self.pAddedSugars = self.portionInfo(food.addedSugars)
+        self.pSatFats = self.portionInfo(food.saturatedFats)
+        self.pUnSatFat = self.portionInfo(food.unsaturatedFats)
+        #self.pServingSize = self.portionInfo(food.serving) Commented out: Will be implemeted later.
+
+    def portionInfo(self, nutritionValue):
+        '''
+        This is where the portion contents will be calculated
+        The method will take in a food value and convert it from its
+        serving size to its portion size
+        returns portioned nutritionValue
+        '''
+    
+    def __str__(self) -> str:
+        '''
+        Returns a list containing all portioned nutritional values
+        '''
