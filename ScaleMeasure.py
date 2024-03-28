1e53cf59efa2d2a7e07c01b62a4afc2949816341
FungusDuder <160258885+CooperBoling@users.noreply.github.com>
2024-03-27 19:11:45 -0500

    Small fixes
    Small fixes and creation of __str__

--- ScaleMeasure.py
+++ ScaleMeasure.py
@@ -3,9 +3,9 @@ class ScaleMeasure():
         self.pWeight = weight
         self.pSugars = self.portionInfo(food.sugars)
         self.pCalories = self.portionInfo(food.calories)
-        self.pProtien = self.portionInfo(food.protien)
-        self.pCarbs = self.portionInfo(food.carbs)         #The "p" in front of the variable names
-        self.pFiber = self.portionInfo(food.fiber)          #stands for "portioned Sugars" etc.
+        self.pProtein = self.portionInfo(food.protien)
+        self.pCarbs = self.portionInfo(food.carbs)              #The "p" in front of the variable names
+        self.pFiber = self.portionInfo(food.fiber)                  #stands for "portioned Sugars" etc.
         self.pAddedSugars = self.portionInfo(food.addedSugars)
         self.pSatFats = self.portionInfo(food.saturatedFats)
         self.pUnSatFat = self.portionInfo(food.unsaturatedFats)
@@ -18,8 +18,10 @@ class ScaleMeasure():
         serving size to its portion size
         returns portioned nutritionValue
         '''
+        return 
     
     def __str__(self) -> str:
         '''
         Returns a list containing all portioned nutritional values
         '''
+        return f"Weight:{self.pWeight}\nSugars:{self.pSugars}\nCalories:{self.pWeight}\Protein:{self.pProtein}\n"
