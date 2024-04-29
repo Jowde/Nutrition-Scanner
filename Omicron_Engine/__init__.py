05f9f6d74645f0041d5102cd665a1a3dc31963ca
Jowde <110619686+JudeBarcelona@users.noreply.github.com>
2024-04-29 18:15:08 -0500

    Made basic GUI in Kivy

--- Omicron_Engine/__init__.py
+++
@@ -1,5 +0,0 @@
-from Omicron_Engine.core.screen import Screen
-from Omicron_Engine.core.main_display import MainDisplay
-from Omicron_Engine.gui_components.menu import Menu
-from Omicron_Engine.gui_components.label import Label
-from Omicron_Engine.gui_components.button import Button
--- main.py
+++ main.py
@@ -1,9 +1,41 @@
-import Omicron_Engine as OE
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 
-if __name__ == "__main__":
-    display = OE.MainDisplay(
-                                current_screen='start_screen', 
-                                display_title='Program 4', 
-                                quick_boot = True
-                             )
-    display.main_loop()
+#Define different screens
+class StartScreen(Screen):
+    pass
+
+class InfoScreen(Screen):
+    pass
+
+class FoodListScreen(Screen):
+    pass
+
+class WeightingScreen(Screen):
+    pass
+
+class MacroTrackerScreen(Screen):
+    pass
+
+class FoodSelectionScreen(Screen):
+    pass
+
+class PhotoScannerScreen(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+#Designate kv design file
+kv = Builder.load_file('design.kv')
+
+class NutritionScanner(App):
+    def build(self):
+        return kv
+    
+if __name__ == '__main__':
+    ns = NutritionScanner()
+    ns.run()
