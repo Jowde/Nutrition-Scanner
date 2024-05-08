eaea0c599e0bc1486229dbf2b8771f9c19210420
BagOfSand <116386156+BagOfSand@users.noreply.github.com>
2024-05-08 07:58:07 -0500

    Split scale into functions
    class also has a debug mode for testing.
    
    Note: scale class does not have a tare() function, since it can only pull from Serial monitor, NOT call arduino functions.

--- scale.py
+++ scale.py
@@ -1,34 +1,61 @@
 import time
 import serial.tools.list_ports
 
-
-
 #TinkerAssist video on importing arduino's serial monitor
+# used as a basis
 
-ports = serial.tools.list_ports.comports()
-serialInstance = serial.Serial()
+# debug variable for prints
+DEBUG = True
 
-portList = []
-for port in ports:
-    portList.append(str(port))
-    print(str(port))
+class Scale:
+    def __init__(self) -> None:
+        self.ports = serial.tools.list_ports.comports()
+        self.serialInstance = serial.Serial()
+        self.find_ports()
 
-val = input("Select port: COM")
+    def find_ports(self):
+        portList = []
+        for port in self.ports:
+            portList.append(str(port))
+            print(str(port))
 
-# go through each port detected
-for port in portList:
-    # if the player chooses a valid port, choose the first one and break
-    if port.startswith("COM"+str(val)):
-        portVar = "COM"+str(val)
-        print(f"{portVar} selected")
-        break
+        # by default, the port number will be 3
+        val = 3
+        # allow user change if it's in debug mode
+        if DEBUG:
+            val = input("Select port: COM")
 
-serialInstance.baudrate = 57600
-serialInstance.port = portVar
-serialInstance.open()
+        # go through each port detected
+        for port in portList:
+            # if the player chooses a valid port, choose the first one and break
+            if port.startswith("COM"+str(val)):
+                portVar = "COM"+str(val)
+                print(f"{portVar} selected")
+                break
 
-while True:
-    if serialInstance.in_waiting:
-        packet = serialInstance.readline()
-        terminalLine = packet.decode("utf").rstrip("\n")
-        print(terminalLine)
+        self.serialInstance.baudrate = 57600
+        self.serialInstance.port = portVar
+        self.serialInstance.open()
+
+    def get_mass(self):
+        while True:
+            if self.serialInstance.in_waiting:
+                packet = self.serialInstance.readline()
+                terminalLine = packet.decode("utf").rstrip("\n")
+                print(terminalLine)
+                if DEBUG:
+                    temp = terminalLine.find("one reading:")
+                    print(f"         {temp}")
+                if terminalLine.find("one reading:")>=0:
+                    mass = terminalLine.rsplit("average:")[-1].strip()
+                    if DEBUG:
+                        print(f"mass={mass}")
+                    return mass
+
+'''
+Test main
+'''
+if DEBUG:
+    scale = Scale()
+    while True:
+        print(scale.get_mass())
